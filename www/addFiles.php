667e3a9e0b7cf87d6f15973585ab1dfb3122b2a4
rodgerb@miamioh.edu <rodgerb@miamioh.edu>
2020-04-30 20:12:12 -0400

    finished file upload for cv and dars

--- www/addFiles.php
+++ www/addFiles.php
@@ -1,20 +1,15 @@
 <?php
 
+include('./front-end/goodbye.html');
+
 if(isset($_POST['submitFiles'])){
 
-	$target_dir = "./back-end/CV/";
-	$cv_file = $target_dir . basename($_FILES["statementFile"]["name"]);
-	$dar_file = $target_dir . basename($_FILES["darFile"]["name"]);
+	$target_dir_cv = "./back-end/CV/";
+	$target_dir_dar = "./back-end/Dars/";
+	$cv_file = $target_dir_cv . basename($_FILES["statementFile"]["name"]);
+	$dar_file = $target_dir_dar . basename($_FILES["darFile"]["name"]);
 
-	if(move_uploaded_file($_FILES["statementFile"]["tmp_name"], $cv_file)) {
-        echo "The file ". basename( $_FILES["statementFile"]["name"]). " has been uploaded.";
-	} 
-
-	if(move_uploaded_file($_FILES["darFile"]["tmp_name"], $dar_file)) {
-        echo "The file ". basename( $_FILES["darFile"]["name"]). " has been uploaded.";
-	} 
-}
-else{
-	echo "File not found";
+	move_uploaded_file($_FILES["statementFile"]["tmp_name"], $cv_file);
+	move_uploaded_file($_FILES["darFile"]["tmp_name"], $dar_file);
 }
 ?>
