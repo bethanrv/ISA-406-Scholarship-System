17436d6d4fc7342fe1e84c3e9a497d9f9bbbebfa
rodgerb@miamioh.edu <rodgerb@miamioh.edu>
2020-05-02 19:24:52 -0400

    Dar and CV file uploads now correspond to unique id

--- www/addFiles.php
+++ www/addFiles.php
@@ -2,14 +2,28 @@
 
 include('./front-end/goodbye.html');
 
+$cv_file = '';
+$dar_file= '';
+$name = $_POST['uid'];
+
+
+
 if(isset($_POST['submitFiles'])){
 
 	$target_dir_cv = "./back-end/CV/";
 	$target_dir_dar = "./back-end/Dars/";
-	$cv_file = $target_dir_cv . basename($_FILES["statementFile"]["name"]);
-	$dar_file = $target_dir_dar . basename($_FILES["darFile"]["name"]);
+	$cv_file = $target_dir_cv . basename($name . '.pdf');
+	$dar_file = $target_dir_dar . basename($name . '.html');
 
 	move_uploaded_file($_FILES["statementFile"]["tmp_name"], $cv_file);
 	move_uploaded_file($_FILES["darFile"]["tmp_name"], $dar_file);
+
+	
 }
+
+
+
+
+
+
 ?>
