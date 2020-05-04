a12c2b7d3e9a75dc69e783be436b00f36c9b0a35
Brian Rodgers Vargo <47480412+bethanrv@users.noreply.github.com>
2020-05-03 20:55:27 -0400

    Moved DAR parsing area to addFiles.php

--- www/addFiles.php
+++ www/addFiles.php
@@ -4,7 +4,7 @@ include('./front-end/goodbye.html');
 
 $cv_file = '';
 $dar_file= '';
-$name = $_POST['uid'];
+$uid = $_POST['uid'];
 
 
 
@@ -12,11 +12,37 @@ if(isset($_POST['submitFiles'])){
 
 	$target_dir_cv = "./back-end/CV/";
 	$target_dir_dar = "./back-end/Dars/";
-	$cv_file = $target_dir_cv . basename($name . '.pdf');
-	$dar_file = $target_dir_dar . basename($name . '.html');
+	$cv_file = $target_dir_cv . basename($uid . '.pdf');
+	$dar_file = $target_dir_dar . basename($uid . '.html');
 
 	move_uploaded_file($_FILES["statementFile"]["tmp_name"], $cv_file);
 	move_uploaded_file($_FILES["darFile"]["tmp_name"], $dar_file);
+
+
+	//read from dars file and save info
+	$dars = fopen("./back-end/Dars/" . $uid . ".html", "r") or die("Unable to open file!");
+
+
+	//save info into the following fields (currently populated with dummy data)
+	$year = "1";
+	$majors = "ISA";
+	$minors = "";
+	$cumGPA = "2.9";
+	$isaGPA = "3.0";
+	$majGPA = "3.0";
+	$isaGrades = "ISA 406: A, ISA 203: B, ISA 102: A"; //could also store in a data structure 
+
+
+	//stores info into student info csv and grades csv
+	$studentInfoFile = fopen("./back-end/studentInfo.csv", "a+") or die("Unable to open file!");
+	$gradesFile = fopen("./back-end/studentGrades.csv", "a+") or die("Unable to open file!");
+
+	fwrite($studentInfoFile, PHP_EOL . "$uid,$year,$majors,$minors,$cumGPA,$isaGPA,$majGPA");
+	fwrite($gradesFile, PHP_EOL . "$uid,$isaGrades");
+
+	fclose($studentInfoFile);
+	fclose($gradesFile);
+	fclose($dars);
 
 	
 }
