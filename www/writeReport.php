8dae9715b1c7331a227d5b44a31c806df0e2ab81
Brian Rodgers Vargo <47480412+bethanrv@users.noreply.github.com>
2020-05-05 19:04:09 -0400

    Added Dars and CV to appropriate report folder

--- www/writeReport.php
+++ www/writeReport.php
@@ -8,6 +8,11 @@ foreach($students as $student){
 	$myfile = fopen("./back-end/Report/". $uid . "/" . $uid . ".csv", "w") or die("Unable to open file!");
 	fwrite($myfile, PHP_EOL . "$student");
 	fclose($myfile);
+
+	//copy dars and cv
+	copy( "./back-end/Dars/" . $uid . ".html","./back-end/Report/" . $uid . "/" . $uid . ".html");
+	copy( "./back-end/CV/" . $uid . ".pdf","./back-end/Report/" . $uid . "/" . $uid . ".pdf");
+
 }
 
 ?>
