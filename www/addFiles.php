164d9c59674671558d86a92c10d0081b108d20e9
Ben Randale <nerdude9816@gmail.com>
2020-05-06 10:40:39 -0400

    Update addFiles.php

--- www/addFiles.php
+++ www/addFiles.php
@@ -54,6 +54,8 @@ if(isset($_POST['submitFiles'])){
 	  $minors = substr($file_string, $minor_index - 30, 36);  // Find the minor, but include extra leading characters
 	  $minors_start = strlen($minors) - strpos(strrev($minors), ">");  // Reverse the string to find the last index of ">"
 	  $minors = substr($minors, $minors_start);
+	  if (substr($minors, 0, 13) = "Requirement: ")
+	    $minors = substr($minors, 13);
 	}
 
         //  Find Graduation Date  //
